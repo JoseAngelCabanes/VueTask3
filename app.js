2305254382b0d33016c61b1032d487fbbcb68c52
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-06 13:45:56 +0200

    exercise completed

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 const app = Vue.createApp({
-
-    data() {
-        return {
-            number: 0,
-            counterMessage: ''
-        };
+  data() {
+    return {
+      number: 0,
+      counterMessage: "",
+    };
+  },
+  watch: {
+    counter() {
+        console.log('Watcher executing...');
+        const that = this;
+        setTimeout(function() {
+            that.number = 0;
+        }, 5000);
+    } 
+  },
+  computed: {
+    counter() {
+      if (this.number < 37) {
+        return "Not there yet!";
+      } else if (this.number === 37) {
+        return this.number;
+      } else {
+        return "Too much!";
+      }
     },
-    computed: {
-        counter() {
-            if (this.number < 37) {
-                return 'Not there yet!';
-            } else if (this.number === 37) {
-                return this.number;
-            } else {
-                return 'Too much!';
-            }
-        }
+  },
+  methods: {
+    add(num) {
+      this.number = this.number + num;
     },
-    methods: {
-        add(num) {
-            this.number = this.number + num;
-        }
-    }
+  },
 });
 
-app.mount('#assignment');
+app.mount("#assignment");
