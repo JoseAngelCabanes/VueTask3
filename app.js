635e7508597f78b582e626a627e925771c4e54a2
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-06 13:23:39 +0200

    added setTimeout and logic operators

--- app.js
+++ app.js
@@ -2,17 +2,48 @@ const app = Vue.createApp({
 
     data() {
         return {
-            counter: 0
+            counter: 0,
+            counterMessage: ''
         };
     },
     watch: {
-
+        counter(value) {
+            if (value != 1) {
+              const that = this;
+              that.counterMessage = ''
+              setTimeout(function() {
+                that.counter = 0;
+              }, 5000);
+            } else {
+                if (value === 1) {
+                    console.log('FIRST IF');
+                    that.counterMessage = ''
+                } else {
+                    if (value ) {
+                        
+                    }
+                }
+            }
+          }
+            // if (value === 37) {
+            //     return 'That is it!'
+            // } else {
+            //     if (value < 37) {
+            //         return 'Not there yet'
+            //     } else {
+            //         if (value > 37) {
+            //             return 'Too much!'
+            //         }
+            //     }
+                
+            // }
+        
     },
     methods: {
         add(num) {
-            this.num = this.counter + num;
+            this.counter = this.counter + num;
         }
-    },
+    }
 });
 
-app.mount('#assignment')
+app.mount('#assignment');
