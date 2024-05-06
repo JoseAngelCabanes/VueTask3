3d045a14a5ffec3744f28caa55297c9976a4c6e4
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-06 13:37:27 +0200

    added all conditionals

--- app.js
+++ app.js
@@ -2,46 +2,24 @@ const app = Vue.createApp({
 
     data() {
         return {
-            counter: 0,
+            number: 0,
             counterMessage: ''
         };
     },
-    watch: {
-        counter(value) {
-            if (value != 1) {
-              const that = this;
-              that.counterMessage = ''
-              setTimeout(function() {
-                that.counter = 0;
-              }, 5000);
+    computed: {
+        counter() {
+            if (this.number < 37) {
+                return 'Not there yet!';
+            } else if (this.number === 37) {
+                return this.number;
             } else {
-                if (value === 1) {
-                    console.log('FIRST IF');
-                    that.counterMessage = ''
-                } else {
-                    if (value ) {
-                        
-                    }
-                }
+                return 'Too much!';
             }
-          }
-            // if (value === 37) {
-            //     return 'That is it!'
-            // } else {
-            //     if (value < 37) {
-            //         return 'Not there yet'
-            //     } else {
-            //         if (value > 37) {
-            //             return 'Too much!'
-            //         }
-            //     }
-                
-            // }
-        
+        }
     },
     methods: {
         add(num) {
-            this.counter = this.counter + num;
+            this.number = this.number + num;
         }
     }
 });
